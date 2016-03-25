2084baa3a4bb9aca9f6d2e49165b826fc02db2db
cncastillo <cncastillo@uc.cl>
2016-03-25 15:24:54 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
-msp430-gcc -Wall -Wextra --std=gnu99 -mmcu=msp430f5529 -Os -o go_msp.elf hello_msp.c
+msp430-gcc -Wall -Wextra --std=gnu99 -mmcu=msp430f5529 -Os -o go_msp.elf hello_msp.c #Cambiar hello_msp.c por nombre código C
 msp430-objcopy --output-target=elf32-msp430 go_msp.elf go_msp.bin
 sudo mspdebug tilib "prog go_msp.bin"
 
