89312373db3e3c64779cec260995ca77fd0d7191
cncastillo <cncastillo@uc.cl>
2016-03-25 15:22:22 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 msp430-gcc -Wall -Wextra --std=gnu99 -mmcu=msp430f5529 -Os -o go_msp.elf hello_msp.c
 msp430-objcopy --output-target=elf32-msp430 go_msp.elf go_msp.bin
-mspdebug tilib "prog go_msp.bin"
+sudo mspdebug tilib "prog go_msp.bin"
 
 rm go_msp.elf
 rm go_msp.bin
